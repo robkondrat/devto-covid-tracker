d56255dfaac0368445a701187abb3eb69f05a3ba
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-07-17 12:33:51 -0500

    fetch covid data w state abbr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as stateAbbr from "datasets-us-states-abbr";
 
 function App() {
+  const [activeQuery, setActiveQuery] = useState("NY")
+  const [stateData, setStateData] = useState({})
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(
+        `https://covidtracking.com/api/states?state=${activeQuery}`
+      );
+      const data = await res.join();
+      setStateData(data);
+    }
+    getData();
+  }, [activeQuery]);
+
+  console.log(stateData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:"600px", textAlign:"center"}}>
+      <h1>COVID-19 Test Tracker</h1>
+      <form>
+        <label>
+          Select a state:
+          <select value={activeQuery} onChange={e => setActiveQuery(e.target.value)}>
+            {Object.values(stateAbbr).map(abbr => {
+              return (
+                <option key={abbr} value={abbr}>
+                  {abbr}
+                </option>
+              )
+            })}
+        </label>
+      </form>
     </div>
   );
 }
